29c2cd7b0bf55f5a508a1dec0ab8bfed8c7b49e5
DivyaDubey06 <meetdivya2006@gmail.com>
2026-06-16 23:41:45 +0530

    Add files via upload

---
+++ DailyTemperatures.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+class DailyTemperatures {
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] result = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() &&
+                    temperatures[i] > temperatures[stack.peek()]) {
+
+                int index = stack.pop();
+                result[index] = i - index;
+            }
+
+            stack.push(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] temperatures = { 73, 74, 75, 71, 69, 72, 76, 73 };
+
+        int[] result = dailyTemperatures(temperatures);
+
+        for (int days : result) {
+            System.out.print(days + " ");
+        }
+    }
+}
