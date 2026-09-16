4e2771f2254ed18da258f1188b8a361a1940f2ba
DivyaDubey06 <meetdivya2006@gmail.com>
2026-09-16 23:28:51 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class palindrome{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String str= sc.nextLine();
+
+        int left=0;
+        int right=str.length()-1;
+
+        while(left<right){
+            if (str.charAt(left) != str.charAt(right)) {
+                System.out.println("Not a palindrome");
+            return;
+            }
+            left++;
+            right--;
+        }
+        System.out.println("Palindrome");
+    
+    }
+}
